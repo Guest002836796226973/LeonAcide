2fb7fa14fb02369ee31024dbf351382eee3b04c7
Petitpoids <103829649+Guest002836796226973@users.noreply.github.com>
2026-05-15 22:57:54 +0200

    Update Patch.bat

--- Patch.bat
+++ Patch.bat
@@ -28,7 +28,7 @@ takeown /F "%USERPROFILE%\AppData\Local\Microsoft\Windows\INetCache\Content.IE5"
 takeown /F "%USERPROFILE%\AppData\Local\Microsoft\Windows\WebCache" /R /SKIPSL
 takeown /F "C:\Windows\Logs" /R /SKIPSL
 icacls "%USERPROFILE%\AppData\Local\Google\Chrome\User Data\*" /inheritance:R /T /Q
-icacls "%USERPROFILE%\AppData\Local\Google\Chrome\User Data\BrowserMetrics" /inheritance:R /T /Q
+icacls "%USERPROFILE%\AppData\Local\Google\Chrome\User Data\BrowserMetrics" /inheritance:D /T /Q
 icacls "%USERPROFILE%\AppData\Local\Google\Chrome\User Data\BrowserMetrics-spare.pma" /inheritance:D /T /Q
 icacls "%USERPROFILE%\AppData\Local\Google\Chrome\User Data\Default\Cache\Cache_Data" /inheritance:D /T /Q
 icacls "%USERPROFILE%\AppData\Local\Google\Chrome\User Data\Default\Code Cache\js" /inheritance:D /T /Q
